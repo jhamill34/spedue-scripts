dfa0c72ebedef3ea5a41daee4d9f542d44ae3f16
Joshua Rasmussen <joshrasmussen34@gmail.com>
2020-01-27 16:17:50 -0800

    fix: upgrade jest

--- packages/spedue-scripts/config/jestSetup.ts
+++ packages/spedue-scripts/config/jestSetup.ts
@@ -1,7 +1,7 @@
-import 'jest'
-
 // For use in gatsby rather than forcing
 // the client to handle it
+declare let jest: any
+
 const localGlobal = global as any
 localGlobal.___loader = {
   enqueue: jest.fn(),
